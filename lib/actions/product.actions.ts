32c29a127c6a0c64935798d14468e3f4d89b1428
Imran Farooq <imran_farooq7@hotmail.com>
2025-01-08 21:08:53 +0500

    get product by slug action added

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -19,3 +19,23 @@ export const getProducts = async () => {
 		};
 	}
 };
+export const getProductBySlug = async (slug: string) => {
+	try {
+		const product = await prisma.product.findFirst({
+			where: {
+				slug,
+			},
+		});
+		const data = convertPrismaObjectToPlain(product);
+		return {
+			data,
+			message: "product fetched successfully",
+		};
+	} catch (error) {
+		console.log(error);
+		return {
+			data: null,
+			message: "faild to fetch product",
+		};
+	}
+};
