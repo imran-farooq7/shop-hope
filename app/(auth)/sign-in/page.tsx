18c331afcea63cdfaf154314bdb73f9779f80826
Imran Farooq <imran_farooq7@hotmail.com>
2025-01-13 20:43:03 +0500

    sign in form completed

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import SignInForm from "@/components/sign-in-form/sign-in-form";
+import React from "react";
+
+const SignInPage = () => {
+	return (
+		<div>
+			<SignInForm />
+		</div>
+	);
+};
+
+export default SignInPage;
---
+++ components/sign-in-form/sign-in-form.tsx
@@ -0,0 +1,102 @@
+import Link from "next/link";
+
+const SignInForm = () => {
+	return (
+		<div className="flex min-h-full flex-1 flex-col justify-center items-center py-12 sm:px-6 lg:px-8">
+			<div className="sm:mx-auto sm:w-full sm:max-w-md">
+				<h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+					Sign in to your account
+				</h2>
+			</div>
+
+			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+				<div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+					<form className="space-y-6" action="#">
+						<div>
+							<label
+								htmlFor="email"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								Email address
+							</label>
+							<div className="mt-2">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									required
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<label
+								htmlFor="password"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								Password
+							</label>
+							<div className="mt-2">
+								<input
+									id="password"
+									name="password"
+									type="password"
+									required
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div className="flex items-center justify-between">
+							<div className="flex items-center">
+								<input
+									id="remember-me"
+									name="remember-me"
+									type="checkbox"
+									className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+								/>
+								<label
+									htmlFor="remember-me"
+									className="ml-3 block text-sm leading-6 text-gray-900"
+								>
+									Remember me
+								</label>
+							</div>
+
+							<div className="text-sm leading-6">
+								<Link
+									href="#"
+									className="font-semibold text-emerald-500 hover:text-emerald-600"
+								>
+									Forgot password?
+								</Link>
+							</div>
+						</div>
+
+						<div>
+							<button
+								type="submit"
+								className="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+							>
+								Sign in
+							</button>
+						</div>
+					</form>
+				</div>
+
+				<p className="mt-10 text-center text-sm text-gray-500">
+					Don't have an account?{" "}
+					<Link
+						href="sign-up"
+						className="font-semibold leading-6 text-emerald-500 hover:text-emerald-600"
+					>
+						Sign up now
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default SignInForm;
